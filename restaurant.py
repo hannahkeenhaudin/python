6aded9e969629e9d585c9d2751645d4ff5dfd4cd
hannahkeenhaudin <72617132+hannahkeenhaudin@users.noreply.github.com>
2021-02-24 12:14:50 +0000

    Update restaurant.py
    Write a program that allows user to enter his favourite starter, main course, dessert and drink. Output a message which says – “Your favourite meal is .........with a glass of....”

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,4 @@
-#1. Write a program that allows user to enter his favourite starter, main course, dessert and drink. Output a message which says – “Your favourite meal is .........with a glass of....”
+#1.Write a program that allows user to enter his favourite starter, main course, dessert and drink. Output a message which says – “Your favourite meal is .........with a glass of....”
 
 #define method and open
 def restaurant():
